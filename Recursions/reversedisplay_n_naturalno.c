767277a4412a647bc5f778a9e7cab941cd802ee2
AdityaSingh1707 <104519385+AdityaSingh1707@users.noreply.github.com>
2022-04-27 22:16:21 +0530

    Add files via upload

---
+++ Recursions/reversedisplay_n_naturalno.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void func(int n)
+{
+    if(n==0)
+        return;
+    else
+    {
+        printf("%d ", n);
+        func(n-1);
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the value of n:\n");
+    scanf("%d", &n);
+    func(n);
+    return 0;
+}
