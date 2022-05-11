7479bc203d774d2cdbec1505d1ea948f4cc299bb
AdityaSingh1707 <104519385+AdityaSingh1707@users.noreply.github.com>
2022-05-11 22:22:14 +0530

    Update linkedlist.h
    Header File including all functions of Linked List.

--- Linked_List/linkedlist.h
+++ Linked_List/linkedlist.h
@@ -1,14 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
+// Header File including all functions of Linked List. 
+
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct st
 {
     int data;
     struct st *next;
-}Node;
+} Node;
 Node *create(int data);
 void append(Node **head, int data);
 void traverse(Node *head);
-int delete(Node **head, int node_number);
+int delete (Node **head, int node_number);
 int add_node_at(Node **head, int node_number, int data);
 int add_node_after(Node **, int node_number, int data);
 int add_node_before(Node **head, int node_number, int data);
+void reverse_linkedlist(Node **Head);
+void rev_order_rec(Node *head);
+void rev_order_iter(Node *head);
