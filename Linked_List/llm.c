58d1e88a520ea2247b5289b9a6fabd96b5787abd
AdityaSingh1707 <104519385+AdityaSingh1707@users.noreply.github.com>
2022-05-11 22:27:18 +0530

    Update llm.c
    Includes all functions declaration defined in Header File.

--- Linked_List/llm.c
+++ Linked_List/llm.c
@@ -1,6 +1,6 @@
 #include "linkedlist.h"
 
-// Function to create a node...
+// function to create a node...
 
 Node *create(int value)
 {
@@ -10,57 +10,57 @@ Node *create(int value)
     return temp;
 }
 
-// Function to append a node to the linked list.... 
+// function to append a node to the linked list....
 
 void append(Node **head, int value)
 {
     Node *new_node = NULL, *temp = *head;
     new_node = create(value);
-    if(temp == NULL){
+    if (temp == NULL)
+    {
         *head = new_node;
         return;
     }
-    while(temp->next != NULL)
+    while (temp->next != NULL)
         temp = temp->next;
     temp->next = new_node;
-
 }
 
-// Function to display (traverse) the linked list  
+// Function to display (traverse) the linked list
 
 void traverse(Node *head)
 {
-    if(head == NULL)
+    if (head == NULL)
     {
         printf("The Linked List is empty.\n");
         return;
     }
-    while(head != NULL)
+    while (head != NULL)
     {
-        printf(" %d --> ",head->data);
+        printf(" %d --> ", head->data);
         head = head->next;
     }
     printf("NULL \n");
 }
 
-// Function to delete a node from linked list... 
+// Function to delete a node from linked list...
 
-int delete(Node **head, int n)
+int delete (Node **head, int n)
 {
     Node *current = *head, *prev;
-    if(n == 1)
+    if (n == 1)
     {
         *head = current->next;
         free(current);
         return 1;
     }
     int i;
-    for(i = 1; current != NULL && i <= n-1; i++)
+    for (i = 1; current != NULL && i <= n - 1; i++)
     {
         prev = current;
         current = current->next;
     }
-    if(current == NULL)
+    if (current == NULL)
         return 0;
     prev->next = current->next;
     free(current);
@@ -71,12 +71,12 @@ int delete(Node **head, int n)
 
 int add_node_at(Node **head, int n, int data)
 {
-    if(n < 1)
+    if (n < 1)
     {
         return 0;
     }
     Node *new = create(data);
-    if(n == 1)
+    if (n == 1)
     {
         new->next = *head;
         *head = new;
@@ -84,12 +84,12 @@ int add_node_at(Node **head, int n, int data)
     }
     int i;
     Node *curr = *head, *prev;
-    for(i = 1; curr != NULL && i < n; i++)
+    for (i = 1; curr != NULL && i < n; i++)
     {
         prev = curr;
         curr = curr->next;
     }
-    if(curr == NULL)
+    if (curr == NULL)
         return 0;
     prev->next = new;
     new->next = curr;
@@ -100,13 +100,59 @@ int add_node_at(Node **head, int n, int data)
 
 int add_node_before(Node **head, int n, int data)
 {
-    return add_node_at(head, n-1, data);
+    return add_node_at(head, n - 1, data);
 }
 
 // Function to add a new node after the nth position in the linked list...
 
 int add_node_after(Node **head, int n, int data)
 {
-    return add_node_at(head, n+1, data);
+    return add_node_at(head, n + 1, data);
 }
+
+// Function to reverse linked list using recursion...
+
+void rev_order_rec(Node *head)
+{
+    if (head != NULL)
+    {
+        rev_order_rec(head->next);
+        printf("%d -> ", head->data);
+    }
+}
+
+// Function to reverse linked list using iteration...
+
+void reverse_linkedlist(Node **Head)
+{
+    Node *curr_node = *Head, *prev_node = NULL, *next_node;
+    while (curr_node != NULL)
+    {
+        next_node = curr_node->next;
+        curr_node->next = prev_node;
+        prev_node = curr_node;
+        curr_node = next_node;
+    }
+    *Head = prev_node;
+}
+
+// Function to display linked list in reverse order (Iteration) ...
+
+void rev_order_iter(Node *head)
+{
+    Node *last, *current;
+    last = NULL;
+    printf("\n");
+    while (head != last)
+    {
+        current = head;
+        while (current->next != last)
+        {
+            current = current->next;
+        }
+        printf("%d -> ", current->data);
+        last = current;
+    }
+}
+
 
