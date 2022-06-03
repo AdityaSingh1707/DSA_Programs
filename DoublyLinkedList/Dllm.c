c9fb50b134734277dade663fe41af95a86c0a98b
AdityaSingh1707 <104519385+AdityaSingh1707@users.noreply.github.com>
2022-06-03 19:32:39 +0530

    Create Dllm.c

---
+++ DoublyLinkedList/Dllm.c
@@ -0,0 +1,24 @@
+#include "dll.h"
+
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+    Node *pos = NULL;
+
+    add_end(&head, &tail, 5);
+    add_end(&head, &tail, 10);
+    add_end(&head, &tail, 50);
+    add_end(&head, &tail, 15);
+    add_end(&head, &tail, 20);
+    add_end(&head, &tail, 25);
+
+    traverse_next(head);
+    traverse_prev(tail);
+
+    pos = search_by_pos(head, 3);
+    add_in_between_after(pos, 30);
+
+    printf("%d", head->data);
+
+    return 0;
+}
